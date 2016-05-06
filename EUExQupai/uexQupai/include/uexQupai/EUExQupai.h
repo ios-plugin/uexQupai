3bd251b544ca1d9e80a163db102d82cf4dc15842
ygcutter <guang.yang@zymobi.com>
2016-05-06 13:34:18 +0800

    优化插件

---
+++ EUExQupai/uexQupai/include/uexQupai/EUExQupai.h
@@ -0,0 +1,13 @@
+//
+//  EUExQupai.h
+//  EUExQupai
+//
+//  Created by 杨广 on 16/4/6.
+//  Copyright © 2016年 杨广. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "EUExBase.h"
+#import <ALBBQuPaiPlugin/ALBBQuPaiPlugin.h>
+@interface EUExQupai : EUExBase
+@end
